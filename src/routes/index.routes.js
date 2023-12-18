48d388f8ed7f13b5615e24f2ae3a003b5b4aae04
JeremiasAvero <jeremiasavero98@gmail.com>
2023-12-18 00:04:55 -0300

    start script added

---
+++ src/routes/index.routes.js
@@ -0,0 +1,5 @@
+const index = (req,res) => {
+    res.json({
+        message: "Welcome to my Users Api. Made with NodeJs, Express, MongoDb and Swagger. Enter to /api-doc to see documentation"
+    })
+}
